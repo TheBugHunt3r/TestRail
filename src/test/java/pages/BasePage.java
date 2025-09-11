41e5bc8a1078833f997975bc0dc8939cedccccd4
Stanislaw <stasgolovnev22@gmail.com>
2025-09-11 16:56:55 +0300

    Added Login Tests

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,5 +1,16 @@
 package pages;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.disappear;
+import static com.codeborne.selenide.Selenide.$;
+
 public class BasePage {
 
+    public final String BASE_URL = "https://saucedemo111234555.testrail.io/index.php?/auth/login/LWZhMDA0NWExYzgyM2Uy" +
+            "NWUzMDVjMjE3ZmRjNzU1ZWEzMjJkOTZmNWY1MWEwODk0MDNjZDJhN2E1NTQ2YTEzM2Y:";
+
+    public BasePage() {
+    }
 }
