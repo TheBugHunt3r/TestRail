6b9601ee7b17432ed8bd29b61c714314c0a05ad1
alisapavluchenkova <sonyapvl666@gmail.com>
2025-09-20 18:09:30 +0300

    refactor of creating projects and elements

---
+++ src/test/java/dto/ProjectFactory.java
@@ -0,0 +1,20 @@
+package dto;
+
+import com.github.javafaker.Faker;
+
+public class ProjectFactory {
+
+    public  static String URL = "https://chat.deepseek.com/%id%";
+
+    public static Project createProject(String defaultAccess) {
+        Faker faker = new Faker();
+        return Project.builder()
+                .name(faker.name().title())
+                .announcement(faker.animal().name())
+                .defect_view_url(URL)
+                .defect_add_url(URL)
+                .reference_view_url(URL)
+                .reference_add_url(URL)
+                .build();
+    }
+}
--- src/test/java/pages/ProjectPage.java
+++ src/test/java/pages/ProjectPage.java
@@ -2,12 +2,15 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 import dto.Elements;
+import dto.Project;
+import dto.ProjectFactory;
 import wrappers.*;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static dto.Elements.*;
 import static wrappers.Checkbox.check;
 import static wrappers.Input.*;
 import static wrappers.IntegrationField.setOption;
@@ -15,8 +18,6 @@ import static wrappers.RadioButton.select;
 
 public class ProjectPage extends BasePage{
 
-    protected SelenideElement LOGO = $x("//div[@data-testid='testCaseContentHeaderTitle']");
-    protected SelenideElement ADD_PROJECT_BUTTON = $x("//a[@data-testid='sidebarProjectsAddButton']");
     public final String PROJECT_URL = "https://saucedemo111234555.testrail.io/index.php?/admin/projects/add/1";
 
 
@@ -32,9 +33,9 @@ public class ProjectPage extends BasePage{
         return this;
     }
 
-    public ProjectPage createProject() {
-        setNormalText("Name", "stas");
-        TextArea.setText("Announcement", "Project");
+    public ProjectPage createProject(Project project, String projectName) {
+        setNormalText("Name", projectName);
+        TextArea.setText("Announcement", project.getAnnouncement());
         check("Show the announcement on the overview page");
         check("Enable Test Case Approvals");
         select("Use multiple test suites to manage cases");
@@ -43,21 +44,31 @@ public class ProjectPage extends BasePage{
         Picklist.select("Default Access", "Tester");
         switchPage("Defects");
         setOption("Defect Plugin", "Asana");
-        setBugText("Defect View Url", "https://saucedemo111234555.testrail.io/index.php?/admin/projects/add/%id%");;
-        setBugText("Defect Add Url", "https://saucedemo111234555.testrail.io/index.php?/admin/projects/add/1");
+        setBugText("Defect View Url", project.getDefect_view_url());;
+        setBugText("Defect Add Url", project.getDefect_add_url());
         switchPage("References");
         setOption("Reference Plugin", "Asana");
-        setBugText("Reference View Url", "https://saucedemo111234555.testrail.io/index.php?/admin/projects/add/%id%");
-        setBugText("Reference Add Url", "https://saucedemo111234555.testrail.io/index.php?/admin/projects/add/1");
+        setBugText("Reference View Url", project.getReference_view_url());
+        setBugText("Reference Add Url", project.getReference_add_url());
         switchPage("User Variables");
-        Elements.ADD_PROJECT_BUTTON.shouldBe(visible);
-        Elements.ADD_PROJECT_BUTTON.click();
-        $x("//div[@class='message message-success']").shouldBe(visible);
+        CREATE_BUTTON.shouldBe(visible);
+        CREATE_BUTTON.click();
+        COMPLETE_LOGO.shouldBe(visible);
         return this;
     }
 
     public ProjectPage switchPage(String label) {
         $x("//div[@class='tab-header']//a[contains(text(), '" + label + "')]").click();
+        return this;
+    }
+
+    public ProjectPage deleteProject(Project project, String projectName) {
+        $x("//tr[@class='header']/following-sibling::tr[@class='odd hoverSensitive']" +
+                "//td/child::a[contains(text(), '" + projectName + "')]//ancestor::tr" +
+                "//a//div[@class='icon-small-delete']").click();
+        DELETE_PROJECT_BUTTON.click();
+        CONFIRM_DELETE_BUTTON.click();
+        DELETE_LOGO.shouldBe(visible);
         return this;
     }
 }
